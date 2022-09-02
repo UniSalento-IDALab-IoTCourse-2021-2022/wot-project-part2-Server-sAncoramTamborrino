67e5efa26a15c7382bea434e93ea5671c9440444
Samuele Ancora <samueleancora@MacBook-Pro-di-Samuele.local>
2022-09-02 14:51:54 +0200

    Added database connection

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb://localhost/TemperatureDB';
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json());
+app.post("/temperature", (req, res, next) => {
+    console.log('Achieved '+req.body.temperature+'°C of body temperature.');
+    var sensor = req.body.sensor;
+    var temperature = req.body.temperature;
+    var timestamp = req.body.timestamp;
+
+
+
+    const client = new MongoClient(uri, {useUnifiedTopology: true});
+    async function run() {
+
+        try {
+
+            await client.connect();
+
+            const database = client.db("TemperatureDB");
+            const temperatureColl = database.collection("temperature");
+            // create a document to be inserted
+            const doc = {
+                sensor: sensor,
+                value: temperature,
+                timestamp: timestamp
+            };
+
+            const result = await temperatureColl.insertOne(doc);
+            console.log(`${result.insertedCount} documents were inserted with the _id: ${result.insertedId}`,);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+    res.sendStatus(200)
+});
